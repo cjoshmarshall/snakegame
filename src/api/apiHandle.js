ef0afd1f7ad08478ab59fc442c3826cc58c6601c
unknown <cjoshmarshall@gmail.com>
2022-05-06 00:29:38 +0530

    fix 1.4

--- src/api/apiHandle.js
+++ src/api/apiHandle.js
@@ -1,6 +1,7 @@
 import axios from "axios"
 
-const BASE_URL="http://localhost:3006/api"
+// const BASE_URL="http://localhost:3006/api"
+const BASE_URL="https://joshssnakegame.herokuapp.com/api"
 
 export const publicRequest=axios.create({
     baseURL:BASE_URL
